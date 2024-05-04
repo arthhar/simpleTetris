a412fc3cafb45d9dfda8740f18debf45bd57efa9
arthhar <108536052+arthhar@users.noreply.github.com>
2024-05-04 15:14:55 +0530

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,16 @@
+package main;
+import javax.swing.*;
+
+public class Main{
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Simple Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        gp.launchGame();
+    }
+}
